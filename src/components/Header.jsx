79b87fdf0ba91ed3a76da8ef10ea945af6fa7cdb
giovani <zaoukgiovani@gmail.com>
2023-10-12 13:10:14 +0300

    second commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,7 @@ import { Link } from 'react-router-dom';
 
 function Header() {
   return (
-    <header className='fixed w-full px-[30px] lg:px-[100px] z-30 h-[100px] lg:h-[140px] flex items-center'>
+    <header className='bg-primary fixed w-full px-[30px] lg:px-[100px] z-30 h-[100px] lg:h-[140px] flex items-center'>
       <div className="flex flex-col lg:flex-row lg:items-center w-full justify-between">
 
         {/* Logo  */}
